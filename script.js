6bd12185b41a44c37143d84db4fc1c2e164e6835
Kasnix <luc.kasnix@gmail.com>
2023-11-04 21:42:44 -0300

    feat: init project

---
+++ script.js
@@ -0,0 +1,24 @@
+const colaboradores = [
+  {
+    matricula: '1',
+    nome: 'Lucas',
+    cargo: 'Desenvolvedor Front-end Sênior',
+  },
+  {
+    matricula: '2',
+    nome: 'Diego',
+    cargo: 'Desenvolvedor Front-end Júnior',
+  },
+];
+
+// Ação CREATE do CRUD
+function contratarColaborador() {}
+
+// Ação READ do CRUD
+function mostrarColaboradores() {}
+
+// Ação UPDATE do CRUD
+function promoverColaborador() {}
+
+// Ação DELETE do CRUD
+function demitirColaborador() {}
